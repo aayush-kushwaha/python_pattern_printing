a5b32a51e0f533df1dee21e8d1c8cb9d5c3ed384
Aayush Kushwaha <1728107@kiit.ac.in>
2022-07-27 13:11:11 +0530

    first_commit

---
+++ pattern6.py
@@ -0,0 +1,6 @@
+k = 1
+for i in range(5):
+    for j in range(5):
+        print(k,end=" ")
+        k += 1
+    print()
---
+++ pattern8.py
@@ -0,0 +1,6 @@
+k = 2
+for i in range(5):
+    for j in range(5):
+        print(k,end=" ")
+        k+= 2
+    print()
